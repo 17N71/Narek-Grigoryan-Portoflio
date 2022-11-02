3512f80a8ee652de219e08975405f770c46d1244
Narek <narek07012020@gmail.com>
2022-11-02 19:37:13 +0400

    changed cursor code in _index.js | cleared _app.js removed target _blank from portfolios links

--- pages/index.js
+++ pages/index.js
@@ -21,44 +21,21 @@ import Media from "react-media";
 import Head from "next/head";
 export const dataContext = createContext(null);
 export default function Page({ data }) {
-  const [activeMouse, setActiveMouse] = React.useState(false);
   const [lang, setLang] = React.useState("en");
   const [menu, setMenu] = React.useState(false);
-  const secondaryCursor = React.useRef(null);
-  const mainCursor = React.useRef(null);
-  const positionRef = React.useRef({
-    mouseX: 0,
-    mouseY: 0,
-    destinationX: 0,
-    destinationY: 0,
-    distanceX: 0,
-    distanceY: 0,
-    key: -1,
-  });
+  const changeLang = useCallback(() => {
+    setLang(lang === "en" ? "hy" : "en");
+    }, [lang]);
+
+    const openMenu = useCallback(() => {
+      setMenu(true);
+    }, [menu]);
+    const closeMenu = useCallback(() => {
+      setMenu(false);
+    }, [menu]);
   React.useEffect(() => {
     document.documentElement.lang = lang;
   }, [lang]);
-  React.useEffect(() => {
-    const KeyDown = (event) => {
-      if (event.which === 27 && !menu) {
-        setMenu(false);
-      }
-    };
-    window.addEventListener("keydown", KeyDown);
-
-    return () => window.removeEventListener("keydown", KeyDown);
-  }, []);
-  const changeLang = useCallback(() => {
-    setLang(lang === "en" ? "hy" : "en");
-  }, [lang]);
-
-  const openMenu = useCallback(() => {
-    setMenu(true);
-  }, [menu]);
-  const closeMenu = useCallback(() => {
-    setMenu(false);
-  }, [menu]);
-
   React.useEffect(() => {
     if (menu) {
       window.document.body.style.paddingRight = "10px";
@@ -68,71 +45,6 @@ export default function Page({ data }) {
       window.document.body.style.paddingRight = "0px";
     }
   }, [menu]);
-  React.useEffect(() => {
-    document.addEventListener("mousemove", (event) => {
-      const { clientX, clientY } = event;
-      const mouseX = clientX;
-      const mouseY = clientY;
-      positionRef.current.mouseX =
-        mouseX - secondaryCursor.current.clientWidth / 2;
-      positionRef.current.mouseY =
-        mouseY - secondaryCursor.current.clientHeight / 2;
-      mainCursor.current.style.transform = `translate3d(${
-        mouseX - mainCursor.current.clientWidth / 2
-      }px, ${mouseY - mainCursor.current.clientHeight / 2}px, 0)`;
-    });
-
-    return () => {};
-  }, []);
-  React.useEffect(() => {
-    const followMouse = () => {
-      positionRef.current.key = requestAnimationFrame(followMouse);
-      const {
-        mouseX,
-        mouseY,
-        destinationX,
-        destinationY,
-        distanceX,
-        distanceY,
-      } = positionRef.current;
-      if (!destinationX || !destinationY) {
-        positionRef.current.destinationX = mouseX;
-        positionRef.current.destinationY = mouseY;
-      } else {
-        positionRef.current.distanceX = (mouseX - destinationX) * 0.1;
-        positionRef.current.distanceY = (mouseY - destinationY) * 0.1;
-        if (
-          Math.abs(positionRef.current.distanceX) +
-            Math.abs(positionRef.current.distanceY) <
-          0.1
-        ) {
-          positionRef.current.destinationX = mouseX;
-          positionRef.current.destinationY = mouseY;
-        } else {
-          positionRef.current.destinationX += distanceX;
-          positionRef.current.destinationY += distanceY;
-        }
-      }
-      secondaryCursor.current.style.transform = `translate3d(${destinationX}px, ${destinationY}px, 0)`;
-    };
-    followMouse();
-  }, []);
-  React.useEffect(() => {
-    function searchRelated(e) {
-      setActiveMouse(true);
-      if (
-        e.toElement.localName === "a" ||
-        e.toElement.localName === "button" ||
-        e.toElement.localName === "li"
-      ) {
-        setActiveMouse(true);
-      } else {
-        setActiveMouse(false);
-      }
-    }
-    window.addEventListener("mousemove", searchRelated);
-    return () => window.removeEventListener("mousemove", searchRelated);
-  }, []);
 
   const MetaTitle = data.data.meta.title;
   const MetaDescription = data.data.meta.description;
@@ -155,7 +67,6 @@ export default function Page({ data }) {
         <meta name="description" content={MetaDescription[lang]} />
         <meta name="og:locale" content={"en_US"} />
         <meta name="og:locale:alternate" content={"hy_AM"} />
-
         <meta name="og:description" content={MetaDescription[lang]} />
         <meta
           name="keywords"
@@ -174,15 +85,7 @@ export default function Page({ data }) {
       <dataContext.Provider
         value={{ lang, data, openMenu, closeMenu, changeLang, menu }}
       >
-        <span className="main-cursor" ref={mainCursor}>
-          <span className="main-cursor-background" />
-        </span>
-        <span
-          className={`secondary-cursor ${activeMouse ? "related" : ""}`}
-          ref={secondaryCursor}
-        >
-          <span className="cursor-background" />
-        </span>
+
         <main className={`${main.main}`}>
           <Media query="(min-width: 1041px)">
             {(matches) => (matches ? "" : <TopSide />)}

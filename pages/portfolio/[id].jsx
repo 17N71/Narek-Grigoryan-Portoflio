d04ba2163f59f23184a126df44aa86ea976b32b4
Narek <narek07012020@gmail.com>
2022-11-04 14:12:10 +0400

    semibold link in [id].jsx

--- pages/portfolio/[id].jsx
+++ pages/portfolio/[id].jsx
@@ -120,7 +120,7 @@ function Portfolio({ data,dataLinks }) {
       }
     }
   };
-  useEffect(() => {
+  useEffect(() =>   {
     if (data.title === "Medley") {
       setAllImages(portfoliosImages.MedelyImages);
     }
@@ -177,9 +177,9 @@ function Portfolio({ data,dataLinks }) {
             rel="noreferrer noopener noindex"
             target="_blank"
             href={data.demo}
-            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+            className="relative text-semibold  inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
                   <span
-                    className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-stone-900 rounded-md group-hover:bg-opacity-0">{dataLinks[lang].demo} </span>
+                    className=" text-semibold relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-stone-900 rounded-md group-hover:bg-opacity-0">{dataLinks[lang].demo} </span>
 
           </a>{dataLinks.or[lang]}  <a
           rel="noreferrer noopener noindex"

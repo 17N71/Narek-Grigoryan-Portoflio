04c24b5997b1e1272b9b6cf98111dbc360faa1a4
Narek <narek07012020@gmail.com>
2022-11-04 13:47:50 +0400

    git config case to false

--- components/Portfolios/portfolios.jsx
+++ components/Portfolios/portfolios.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 import Head from "next/head";
 import Image from "next/image";

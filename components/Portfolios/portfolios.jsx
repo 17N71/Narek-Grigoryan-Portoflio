bd38c498470a01718c671a4a7043c80346de9b8c
Narek <narek07012020@gmail.com>
2022-11-04 13:34:15 +0400

    change ESLint rules and rejected [id].jsx into folder Portfolio

--- components/Portfolios/portfolios.jsx
+++ components/Portfolios/portfolios.jsx
@@ -68,7 +68,7 @@ function Portfolios({ allPortfolios }) {
         >
           {allPortfolios.AllPortfolios.map(({ title, id, alt }) => (
             <SplideSlide key={id}>
-              <Link href={`/portfolio/${id}`} className={" cursor-ew-resize"}>
+              <Link href={`/${id}`} className={"cursor-ew-resize"}>
                 <Image
                   style={{ objectFit: "cover", width: "100%", height: "100%" }}
                   className={"pointer-events-none cursor-ew-resize"}
